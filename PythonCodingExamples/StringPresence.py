aa45f2b206329ef8835bbe4d383ca3f4678a21ee
Diego Flores <ldiego.flores98@gmail.com>
2023-04-19 23:28:57 -0700

    Added StringPresence.py

---
+++ PythonCodingExamples/StringPresence.py
@@ -0,0 +1,13 @@
+# The find() method finds the first occurrence of the specified value.
+# The find() method finds the first occurrence of the specified value.
+
+str = "welcome to python programming"
+sub_string = "python"
+
+print(str.find(sub_string))
+
+if str.find(sub_string) == -1:
+    print("Not found")
+else:
+    print("Found")
+
