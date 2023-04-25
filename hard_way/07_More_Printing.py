6d9cfd25abc4ed7cfcb4db4a24dfc2bdc69b3b1e
Diego Flores <ldiego.flores98@gmail.com>
2023-04-24 21:14:25 -0700

    Added 07_More_Printing.py

---
+++ hard_way/07_More_Printing.py
@@ -0,0 +1,21 @@
+print("Mary had a little lamb.")
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere that Mary went.")
+print("." * 10)  # what's that do?
+
+ends1 = "C"
+ends2 = "h"
+ends3 = "e"
+ends4 = "e"
+ends5 = "s"
+ends6 = "e"
+ends7 = "B"
+ends8 = "u"
+ends9 = "r"
+ends10 = "g"
+ends11 = "e"
+ends12 = "r"
+
+# watch end = ' ' at the end. try removing it to see what happens
+print(ends1 + ends2 + ends3 + ends4 + ends5 + ends6, end=' ')
+print(ends7 + ends8 + ends9 + ends10 + ends11 + ends12)
