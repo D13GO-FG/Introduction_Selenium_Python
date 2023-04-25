882d02b97b33750f341bfd157c5c128231d7907a
Diego Flores <ldiego.flores98@gmail.com>
2023-04-24 19:33:44 -0700

    Added 05_More_Variables_And_Printing.py

---
+++ hard_way/05_More_Variables_And_Printing.py
@@ -0,0 +1,19 @@
+my_name = 'Luis Diego Flores Gonzalez'
+my_age = 25  # not a lie
+my_height = 170  # inches
+my_weight = 65  # lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}")
+print(f"He's {my_height} cms tall.")
+print(f"He's {my_weight} kgs heavy")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffe.")
+
+# This line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
